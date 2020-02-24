b0a99dd6429936fb86109c70a59936e8af7002ab
Q Wang <wangq@jfrog.com>
2020-02-24 22:04:07 +0800

    fix gateway in k8s

--- runAllinK8s.sh
+++ runAllinK8s.sh
@@ -2,18 +2,21 @@
 
 kubectl delete -f kube-deploy/discovery.yaml
 kubectl delete -f kube-deploy/config.yaml
-#kubectl delete -f kube-deploy/notebook.yaml
+kubectl delete -f kube-deploy/notebook.yaml
 
 cd config
 ./buildAndPushImage.sh
 cd ../discovery
 ./buildAndPushImage.sh
 #
-#cd ../notebook
-#./buildAndPushImage.sh
+cd ../notebook
+./buildAndPushImage.sh
+
+cd ../gateway/
+./buildAndPushImage.sh
 
 cd ..
-
-kubectl create -f kube-deploy/discovery.yaml
 kubectl create -f kube-deploy/config.yaml
-#kubectl create -f kube-deploy/notebook.yaml
+kubectl create -f kube-deploy/discovery.yaml
+kubectl create -f kube-deploy/notebook.yaml
+kubectl create -f kube-deploy/gateway.yaml
